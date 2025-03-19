8ba335a23fac8c63ee348c006324e5071cd4a0e8
webbyshow <webinbox4fb@gmail.com>
2025-03-19 18:41:28 +0700

    config.py commit

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+import pyrealsense2 as rs
+
+def setup_realsense(width,height):
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
+    pipeline.start(config)
+    return pipeline
+
+CLASS_NAMES = ['fixed_obstacle', 'moving_obstacles', 'target']
+
+RGB_FOLDER_PATH =   r"data\recorded_frames20250317_003502\color_no_label"
+DEPTH_FOLDER_PATH = r"data\recorded_frames20250317_003502\depth_colormap"
+
+YOLO_MODEL_PATH = r"model\yolov8n.pt"
