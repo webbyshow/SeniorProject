c0c6f892625789d9ec2a43d282fb6da72dcb5a25
webbyshow <webinbox4fb@gmail.com>
2025-04-10 00:59:42 +0700

    Code on Web's macbook

--- CheckGithub.py
+++ CheckGithub.py
@@ -10,3 +10,4 @@ RGB_FOLDER_PATH =   r"data\recorded_frames20250317_002305\color_no_label"
 name = RGB_FOLDER_PATH.split('\\')
 file_unique_name = name[1].split('recorded_frames')[1]
 print(file_unique_name)
+print('this is new code on Web macbook')
