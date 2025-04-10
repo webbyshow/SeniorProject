af760f999eba04c624ea997c23600127a46787d5
Adiraja Sadaeng <webinbox4fb@gmail.com>
2025-04-10 19:43:48 +0700

    Clean up

--- new_Main.py
+++ new_Main.py
@@ -1,5 +1,5 @@
-import cv2
 import os
+import cv2
 import torch
 import numpy as np
 from datetime import datetime
@@ -18,15 +18,14 @@ from new_get_3d_coordinates import get_3D_coordinates_and_rgb
 from new_plan_path_with_3d_point_cloud import detect_obstacles_and_plan_path
 
 def main():
-    model_path = "yolov8n.pt"
+    model_path = "model\yolov8n.pt"
 
     width = 640
     height = 480
     FOV = 48
-    if width * height < 600000:
-        FOV = 48
-    else:
-        FOV = 64
+
+    if width * height < 600000: FOV = 48
+    else: FOV = 64
     
     bag_file = "data\output_20250323_165929-001.bag"
 
@@ -36,19 +35,18 @@ def main():
     # IMPORTANT: Enable reading from .bag file
     config.enable_device_from_file(bag_file, repeat_playback=False)
 
-    MAX_DISTANCE_M = 5.0  # Maximum distance to consider (in meter)
-    MAX_DISTANCE_MM = int(MAX_DISTANCE_M * 1000)  # Convert meters to mm
-    CONF_THRESHOLD = 0.05  # Minimum confidence for YOLO detections
+    MAX_DISTANCE_M = 5.0                                 # Maximum distance to consider (in meter)
+    MAX_DISTANCE_MM = int(MAX_DISTANCE_M * 1000)         # Convert meters to mm
+    CONF_THRESHOLD = 0.05                                # Minimum confidence for YOLO detections
     OBJ_MIN_AREA = (500 * width * height) / (640 * 480)  # Minimum area for depth-based contour detection
-    SAFE_DISTANCE = 1  # Distance in mm to be considered safe for moving forward
-    ANGLE_STABILIZATION = 5  # Prevent angle fluctuation by allowing minor variations
-    prev_turn_angle = 0  # Initialize the previous turn angle
+    SAFE_DISTANCE = 1                                    # Distance in mm to be considered safe for moving forward
+    ANGLE_STABILIZATION = 5                              # Prevent angle fluctuation by allowing minor variations
+    prev_turn_angle = 0                                  # Initialize the previous turn angle
 
     config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)  # RGB Color Camera
 
 # เริ่มต้นการใช้งาน
-    
     profile = pipeline.start(config)
     #config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
     #config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
@@ -73,8 +71,7 @@ def main():
     model = load_yolo_model(model_path)
     
     try:
-        if realtime == 0:
-            loop = iter(lambda: pipeline.wait_for_frames(), None)
+        if realtime == 0: loop = iter(lambda: pipeline.wait_for_frames(), None)
 
         elif realtime == 1:
             depth_folder_path = "D:/EE Engineer/Year4/project/main/recorded_frames20250317_092415/depth_colormap"  # Path to the folder containing saved depth frames
@@ -83,6 +80,8 @@ def main():
             color_frame_files = sorted(os.listdir(color_folder_path))
             loop = zip(depth_frame_files, color_frame_files)
         
+        # This loop below is where we start the command
+
         for frames in loop:
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
@@ -98,8 +97,7 @@ def main():
             x = np.linspace(0, depth_intrinsics.width - 1, depth_intrinsics.width)
             y = np.linspace(0, depth_intrinsics.height - 1, depth_intrinsics.height)
             
-            if depth_image is None or color_image is None:
-                continue
+            if depth_image is None or color_image is None: continue
                 
             depth_colormap, depth_8bit,inpainted_depth,depth_display = process_depth_image(depth_image, MAX_DISTANCE_MM)
             detected_contours = detect_contours(depth_8bit, OBJ_MIN_AREA)
@@ -128,14 +126,12 @@ def main():
                 rgb_out.write(color_image)
                 depth_out.write(depth_colormap)
                 rgb_no_label_out.write(color_no_label)
-            else:
-                pass
+            else: pass
 
             cv2.imshow('RGB Camera (Obstacle Detection)', color_image)
             cv2.imshow('Depth Map', depth_display)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            if cv2.waitKey(1) & 0xFF == ord('q'): break
     finally:
         if realtime == 0:
             pipeline.stop()
@@ -143,6 +139,7 @@ def main():
             rgb_out.release()
             depth_out.release()
             rgb_no_label_out.release()
+
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
